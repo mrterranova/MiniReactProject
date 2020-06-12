02f63b8bf17f8e55c123076e91b00d2fbbbf6ee8
Michal Terranova <MTerranova@talentpath.com>
2020-06-12 14:23:34 -0400

    Updates

---
+++ precache-manifest.374ed9063dbbdc4f23a356f0cfa5ea90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1507c044e576b5892121d75bcd4e2ae9",
+    "url": "/MiniReactProject/index.html"
+  },
+  {
+    "revision": "c84e0c53c35167dcfd5a",
+    "url": "/MiniReactProject/static/css/main.d979a7e0.chunk.css"
+  },
+  {
+    "revision": "602882c87d271a1ca5c4",
+    "url": "/MiniReactProject/static/js/2.b8128578.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MiniReactProject/static/js/2.b8128578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84e0c53c35167dcfd5a",
+    "url": "/MiniReactProject/static/js/main.07cf3a28.chunk.js"
+  },
+  {
+    "revision": "c97951646cf77b79807a",
+    "url": "/MiniReactProject/static/js/runtime-main.a0051c20.js"
+  }
+]);
